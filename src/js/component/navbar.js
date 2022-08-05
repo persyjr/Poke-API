ebd8b7eb84d24b8ed35e16fb1aa030dad6dc10eb
Alejandro <eadelgadom@gmail.com>
2022-08-05 18:27:57 +0000

    con los cambios

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -10,7 +10,7 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light mb-3 justify-content-center ">			
 			<Link to="/">							
 				<span className="navbar-brand mb-0 h1 "><img className="logoStarWars d-flex justify-content-center " src="https://1000marcas.net/wp-content/uploads/2020/01/Logo-Pokemon.png" ></img></span>
-				<strong className="d-flex justify-content-center ">clicK to home</strong>	
+				<strong className="d-flex justify-content-center ">Home</strong>	
 			</Link>			
 		</nav>
 	);
