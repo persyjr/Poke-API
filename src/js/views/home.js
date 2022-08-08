1b01466afd86099bd2a59ab0971a23937e31e5a0
Alejandro <eadelgadom@gmail.com>
2022-08-08 02:20:19 +0000

    imagen portada responsive

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,10 +2,10 @@ import React from "react";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
+	<div className="text-center justify-content-center mt-5">
 		<h1 className="display-1">POKEMON API</h1>
-		<p>
-			<img className="imagenPortada fluid" src="https://c4.wallpaperflare.com/wallpaper/772/372/914/pokemon-eevee-pokemon-eeveelutions-espeon-pokemon-wallpaper-preview.jpg" />
-		</p>
+		
+		<img className="imagenPortada d-flex " src="https://c4.wallpaperflare.com/wallpaper/772/372/914/pokemon-eevee-pokemon-eeveelutions-espeon-pokemon-wallpaper-preview.jpg" />
+		
 	</div>
 );
